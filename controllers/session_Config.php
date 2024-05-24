ee9977d023b7cd4e0c63da3a07ceca7be50411ea
Major_Developer <126969659+MajorDev12@users.noreply.github.com>
2024-05-24 23:35:49 +0300

    Remove sensitive directories from tracking

--- controllers/session_Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Check if the user is not logged in
-if (!isset($_SESSION['adminID']) || !isset($_SESSION['Username'])) {
-    // Redirect to the login page
-    header("location: login.php");
-    exit();
-}
